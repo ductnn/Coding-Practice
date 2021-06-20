cb86dd288621387f7a3d662374ade3e3a26c324b
ductnn <trannhuduc531998@gmail.com>
2021-06-20 17:44:08 +0700

    Test

--- 00.two_sum/two_sum.py
+++ 00.two_sum/two_sum.py
@@ -18,8 +18,22 @@ class Solution:
             values[num] = i
         return []
 
+    # Sorted test
+    def twoSum2(self, nums, target):
+        i = 0
+        j = len(nums) - 1
+
+        while i < j:
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            elif nums[i] + nums[j] > target:
+                j -= 1
+            else:
+                i +=1
+        return []
+
 if __name__ == '__main__':
     solution = Solution()
     nums = list([2, 7, 11, 15])
     target = 9
-    print(solution.twoSum1(nums, target))
+    print(solution.twoSum2(nums, target))
