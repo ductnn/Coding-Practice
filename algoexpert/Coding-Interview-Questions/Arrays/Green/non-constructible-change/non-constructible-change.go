156bca72de6907f0d7f67f259fd16ba5524ae7ec
ductnn <trannhuduc531998@gmail.com>
2023-05-07 13:36:40 +0700

    update: sol NonConstructibleChange

--- algoexpert/Coding-Interview-Questions/Arrays/Green/non-constructible-change/non-constructible-change.go
+++ algoexpert/Coding-Interview-Questions/Arrays/Green/non-constructible-change/non-constructible-change.go
@@ -1,6 +1,40 @@
 package main
 
+import (
+	"fmt"
+)
+
 func NonConstructibleChange(coins []int) int {
 	// Write your code here.
-	return -1
+	bubbleSort(coins)
+	currentCoinCreated := 0
+
+	for _, coin := range coins {
+		if coin-currentCoinCreated > 1 {
+			break
+		}
+
+		currentCoinCreated += coin
+	}
+
+	return currentCoinCreated + 1
+}
+
+func bubbleSort(array []int) []int {
+	// Simple
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-1-i; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+
+	return array
+}
+
+func main() {
+	coins := []int{5, 7, 1, 1, 2, 3, 22}
+
+	fmt.Println(NonConstructibleChange(coins))
 }
