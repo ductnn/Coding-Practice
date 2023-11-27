6bcb66bf006d681c5f1408880b52b7d6eb608ae4
ductnn <trannhuduc531998@gmail.com>
2023-11-28 00:09:06 +0700

    update sol

--- leetcode/01.two_sum/two_sum.go
+++ leetcode/01.two_sum/two_sum.go
@@ -19,8 +19,24 @@ func twoSum(nums []int, target int) []int {
 	return results
 }
 
+func twoSum1(nums []int, target int) []int {
+	m := map[int]int{}
+
+	for i := range nums {
+		temp := nums[i]
+		search := target - temp
+
+		if j, ok := m[search]; ok {
+			return []int{i, j}
+		}
+
+		m[temp] = i
+	}
+	return []int{}
+}
+
 func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 9
-	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSum1(nums, target))
 }
