733b5deffcd58df4e5c994afcc80eb3eb6cec2bd
ductnn <trannhuduc531998@gmail.com>
2023-12-01 19:24:30 +0700

    update

--- leetcode/1876.SubstringsOfSizeThreeWithDistinctCharacters/substringsOfSizeThreeWithDistinctCharacters.go
+++ leetcode/1876.SubstringsOfSizeThreeWithDistinctCharacters/substringsOfSizeThreeWithDistinctCharacters.go
@@ -6,8 +6,11 @@ import (
 
 func countGoodSubstrings(s string) int {
 	// s = "xyzzaz"
-	count := 0
+	if len(s) < 3 {
+		return 0
+	}
 
+	count := 0
 	for i := 0; i < len(s)-2; i++ {
 		if s[i] != s[i+1] && s[i+1] != s[i+2] && s[i] != s[i+2] {
 			count++
