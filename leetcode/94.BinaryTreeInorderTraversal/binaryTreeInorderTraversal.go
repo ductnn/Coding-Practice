d2c2b95522ae408c3252c4e2c8e5f9a9c189632c
ductnn <trannhuduc531998@gmail.com>
2023-12-09 23:01:06 +0700

    add sol

---
+++ leetcode/94.BinaryTreeInorderTraversal/binaryTreeInorderTraversal.go
@@ -0,0 +1,41 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	for root != nil {
+		if root.Left == nil {
+			result = append(result, root.Val)
+			root = root.Right
+		} else {
+			prev := root.Left
+			for prev.Right != nil && prev.Right != root {
+				prev = prev.Right
+			}
+			if prev.Right == nil {
+				prev.Right = root
+				root = root.Left
+			} else {
+				result = append(result, root.Val)
+				prev.Right = nil
+				root = root.Right
+			}
+		}
+	}
+	return result
+}
+
+func main() {}
