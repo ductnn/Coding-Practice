2ffe6a50e18590f768b5d9aeecf1a036a5645ad1
ductnn <trannhuduc531998@gmail.com>
2023-12-17 00:23:34 +0700

    add sol

---
+++ leetcode/242.ValidAnagram/validAnagram.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	cnt := [26]int{}
+	for i := 0; i < len(s); i++ {
+		cnt[s[i]-'a']++
+		cnt[t[i]-'a']--
+	}
+	for _, v := range cnt {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	s := "anagram"
+	t := "nagaram"
+	fmt.Println(isAnagram(s, t))
+}
