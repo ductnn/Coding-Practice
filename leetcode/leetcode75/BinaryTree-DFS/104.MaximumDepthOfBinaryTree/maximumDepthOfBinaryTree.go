94a13f3453b22f77301ff26eeb7804946e09b088
ductnn <trannhuduc531998@gmail.com>
2023-12-17 18:08:50 +0700

    add sol

---
+++ leetcode/leetcode75/BinaryTree-DFS/104.MaximumDepthOfBinaryTree/maximumDepthOfBinaryTree.go
@@ -0,0 +1,35 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	return 1 + max(left, right)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {}
