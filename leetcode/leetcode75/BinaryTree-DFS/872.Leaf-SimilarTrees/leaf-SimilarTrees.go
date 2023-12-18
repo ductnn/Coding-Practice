c40d010c09f4c5b7e36587c459e53b79c0a7cf2f
ductnn <trannhuduc531998@gmail.com>
2023-12-18 10:55:18 +0700

    update

--- leetcode/leetcode75/BinaryTree-DFS/872.Leaf-SimilarTrees/leaf-SimilarTrees.go
+++ leetcode/leetcode75/BinaryTree-DFS/872.Leaf-SimilarTrees/leaf-SimilarTrees.go
@@ -53,8 +53,40 @@ func dfs(root *TreeNode, arr *[]int) {
 
 func main() {
 	// Define the trees
-	root1 := &TreeNode{Val: 3, Left: &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 7}, Right: &TreeNode{Val: 4}}}, Right: &TreeNode{Val: 1, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 8}}}
-	root2 := &TreeNode{Val: 3, Left: &TreeNode{Val: 5, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 7, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 4}}}, Right: &TreeNode{Val: 1, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 9}}}
+	root1 := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val:  5,
+			Left: &TreeNode{Val: 6},
+			Right: &TreeNode{
+				Val:   2,
+				Left:  &TreeNode{Val: 7},
+				Right: &TreeNode{Val: 4},
+			},
+		},
+		Right: &TreeNode{
+			Val:   1,
+			Left:  &TreeNode{Val: 9},
+			Right: &TreeNode{Val: 8},
+		},
+	}
+	root2 := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val:  5,
+			Left: &TreeNode{Val: 6},
+			Right: &TreeNode{
+				Val:   7,
+				Left:  &TreeNode{Val: 2},
+				Right: &TreeNode{Val: 4},
+			},
+		},
+		Right: &TreeNode{
+			Val:   1,
+			Left:  &TreeNode{Val: 4},
+			Right: &TreeNode{Val: 9},
+		},
+	}
 
 	// Check leaf similarity
 	result := leafSimilar(root1, root2)
