4bfbe280572ab99c7075805e545159b04916b9df
ductnn <trannhuduc531998@gmail.com>
2024-01-08 11:21:55 +0700

    add sol

---
+++ leetcode/938.RangeSumOfBST/rangeSumOfBST.go
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/range-sum-of-bst/
+
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rangeSumBST(root *TreeNode, low int, high int) int {
+	if root == nil {
+		return 0
+	}
+	if low <= root.Val && root.Val <= high {
+		return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+	} else if root.Val < low {
+		return rangeSumBST(root.Right, low, high)
+	} else {
+		return rangeSumBST(root.Left, low, high)
+	}
+}
