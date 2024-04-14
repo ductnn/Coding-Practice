7f1ae86935bb8063b5dacdf1b75914d158edaeba
ductnn <trannhuduc531998@gmail.com>
2024-04-14 17:54:26 +0700

    add sol

---
+++ leetcode/404.SumofLeftLeaves/sol.go
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/sum-of-left-leaves
+
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	res := 0
+	if root.Left != nil && root.Left.Left == nil && root.Left.Right == nil {
+		res += root.Left.Val
+	}
+	res += sumOfLeftLeaves(root.Left)
+	res += sumOfLeftLeaves(root.Right)
+	return res
+}
