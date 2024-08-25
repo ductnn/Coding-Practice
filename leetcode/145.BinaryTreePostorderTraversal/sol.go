0cf1c581046672a84bc0057aefdc6a8dacfac416
ductnn <trannhuduc531998@gmail.com>
2024-08-25 23:56:05 +0700

    add sol

---
+++ leetcode/145.BinaryTreePostorderTraversal/sol.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	res := []int{}
+	res = append(res, postorderTraversal(root.Left)...)
+	res = append(res, postorderTraversal(root.Right)...)
+	res = append(res, root.Val)
+
+	return res
+}
+
+func main() {
+	root := &TreeNode{Val: 1}
+	root.Right = &TreeNode{Val: 2}
+	root.Right.Left = &TreeNode{Val: 3}
+
+	result := postorderTraversal(root)
+	fmt.Println(result)
+}
